e2943960efd1c82b566a76136925bb4002c86057
Baboo81 <magicbaboo@gmail.com>
2024-11-25 15:57:04 +0100

    main.js + added img

---
+++ asset/script/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//Logo animation:
+
+const logo = document.getElementById('logo');
+
+let leftPos = 0;
+let upDownPos = 0;
+let dir = -1;
+let current_rotation = 0; 
+
+logo.style.position = "absolute";
+
+function ballAnimate1 () {
+
+    if (leftPos == 1200) {
+        dir = 1
+    } else if 
+        (leftPos == -100) {
+        dir = -1
+    }
+    
+    leftPos += -2 * dir;
+    current_rotation += 1;
+    
+
+    logo.style.left = `${leftPos}px`;
+    logo.style.transform = 'rotate('  + current_rotation + 'deg)';
+
+    requestAnimationFrame(ballAnimate1);
+};
+
+requestAnimationFrame(ballAnimate1);
