42e7adf6e6e67a3b473734c421db63f5e78517d9
Baboo81 <magicbaboo@gmail.com>
2024-11-26 16:09:37 +0100

    Footer, main content

--- asset/script/main.js
+++ asset/script/main.js
@@ -32,3 +32,19 @@ function ballAnimate1 () {
 
 requestAnimationFrame(ballAnimate1);
 ///////////////////// END ////////////////////////////
+
+//Footer (map):
+//Create map:
+const centroid = [50.709925, 4.352603];
+const map = L.map('mapid').setView(centroid, 16.4);
+
+//Add tiles & marker:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([50.709925, 4.352603]).addTo(map);
+
+function openLink (event) {
+    const link = "https://booking.optios.net/19311";
+    window.open(link, "https://soinsdesoie.be", "_self");
+    event.preventDefault();
+}
+///////////////////////// END //////////////////////////
